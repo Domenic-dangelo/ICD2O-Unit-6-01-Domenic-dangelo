5a9f365b1e327e6e5e443c76822031b355b95427
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-13 16:53:48 +0000

    static.yml

--- js/script.js
+++ js/script.js
@@ -1,5 +1,62 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 domeic dangelo All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-6-01-Domenic-dangelo/sw.js", {
+    scope: "/ICD2O-Unit-6-01-Domenic-dangelo/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+
+/**
+ * This function checks if the user has selected the random number from above.
+ */
+// eslint-disable-next-line no-unused-vars
+function calculate() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const lengthA = parseFloat(document.getElementById("sideA").value)
+  const lengthB = parseFloat(document.getElementById("sideB").value)
+  const lengthC = parseFloat(document.getElementById("sideC").value)
+
+  // using the cosine law
+  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
+  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
+  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+
+  console.log(angleA)
+  console.log(angleB)
+  console.log(angleC)
+  console.log(sumOfAngles)
+
+  if (sumOfAngles == 180) {
+    if ((angleA == angleB) && (angleA == angleC)) {
+      document.getElementById("result").innerHTML =
+      'This is an equilateral  triangle!'
+    } 
+    else if ((angleA == angleB) || (angleA == angleC) || (angleB == angleC)) {
+      document.getElementById("result").innerHTML =
+      'This is an isosceles triangle!'
+    } 
+    else {
+      document.getElementById("result").innerHTML =
+      'This is a scalene triangle!'
+    }
+  } 
+  else {
+    document.getElementById("result").innerHTML =
+    'This is not a triangle!'
+  }
+}
