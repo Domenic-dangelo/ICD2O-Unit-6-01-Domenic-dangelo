e4fa02eedbec930b8650164888b98dda3551c0b8
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-13 17:12:24 +0000

    linter fix

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICD2O-Unit-6-01-Domenic-dangelo/sw.js", {
-    scope: "/ICD2O-Unit-6-01-Domenic-dangelo/",
+  navigator.serviceWorker.register('/ICD2O-Unit-6-01-Domenic-dangelo/sw.js', {
+    scope: '/ICD2O-Unit-6-01-Domenic-dangelo/'
   })
 }
 
@@ -23,16 +23,16 @@ if (navigator.serviceWorker) {
  * This function checks if the user has selected the random number from above.
  */
 // eslint-disable-next-line no-unused-vars
-function calculate() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-  const lengthA = parseFloat(document.getElementById("sideA").value)
-  const lengthB = parseFloat(document.getElementById("sideB").value)
-  const lengthC = parseFloat(document.getElementById("sideC").value)
+function calculate () {
+  document.getElementById('hello-world').innerHTML = '<p>Hello, World!</p>'
+  const lengthA = parseFloat(document.getElementById('sideA').value)
+  const lengthB = parseFloat(document.getElementById('sideB').value)
+  const lengthC = parseFloat(document.getElementById('sideC').value)
 
   // using the cosine law
-  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
-  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
-  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+  const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
+  const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI)
+  const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
 
   const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
 
@@ -43,20 +43,17 @@ function calculate() {
 
   if (sumOfAngles == 180) {
     if ((angleA == angleB) && (angleA == angleC)) {
-      document.getElementById("result").innerHTML =
+      document.getElementById('result').innerHTML =
       'This is an equilateral  triangle!'
-    } 
-    else if ((angleA == angleB) || (angleA == angleC) || (angleB == angleC)) {
-      document.getElementById("result").innerHTML =
+    } else if ((angleA == angleB) || (angleA == angleC) || (angleB == angleC)) {
+      document.getElementById('result').innerHTML =
       'This is an isosceles triangle!'
-    } 
-    else {
-      document.getElementById("result").innerHTML =
+    } else {
+      document.getElementById('result').innerHTML =
       'This is a scalene triangle!'
     }
-  } 
-  else {
-    document.getElementById("result").innerHTML =
+  } else {
+    document.getElementById('result').innerHTML =
     'This is not a triangle!'
   }
 }
